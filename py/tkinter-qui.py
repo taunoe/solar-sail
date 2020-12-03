2611ac4e77443b89b69056810f7a944c65ab3142
Tauno Erik <sedumacre@gmail.com>
2020-12-03 18:43:11 +0200

    gui fixes

--- py/tkinter-qui.py
+++ py/tkinter-qui.py
@@ -19,7 +19,6 @@ import time
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-
 def find_numbers(str):
   """
     Extract all the numbers from the given string.
@@ -43,11 +42,18 @@ def find_ports():
     avaible_ports.append(port[0]) # /dev/ttyACM0
   return avaible_ports
 
+def create_serial():
+  global ser
+  ser = serial.Serial()
+  # print(ser)
 
 def open_serial(port, baud):
-  global ser
-  ser = serial.Serial(port, baud, timeout=3)
-  #print("Serial is open: {}".format(ser.isOpen()))
+  ser.port = port
+  ser.baudrate = baud
+  ser.timeout = 3
+  ser.open()
+  print("Serial is open: {}".format(ser.is_open))
+  time.sleep(1.5)
 
 def list_to_matrix(list):
   # https://stackoverflow.com/questions/3636344/read-flat-list-into-multidimensional-array-matrix-in-python
@@ -64,17 +70,19 @@ def read_serial():
   # Send command to Arduino
   ser.write(b'a')
   incoming_data = ser.readline()[:-2].decode('ascii')
-  numbers = find_numbers(incoming_data)
-  return numbers
+  
+  if type(incoming_data) == str:
+    numbers = find_numbers(incoming_data)
+    return numbers
+  return 0
 
 def create_init_figure() -> Figure:
   matrix = [[0,0,0,0,0,0],
-  [0,0,0,0,0,0],
-  [0,0,0,0,0,0],
-  [0,0,0,0,0,0],
-  [0,0,0,0,0,0],
-  [0,0,0,0,0,0]]
-
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0],
+            [0,0,0,0,0,0]]
   # plot the data
   figure = Figure(figsize=(6, 8))
   ax = figure.subplots()
@@ -82,106 +90,103 @@ def create_init_figure() -> Figure:
   return figure
 
 def create_figure() -> Figure:
-  # generate some data
   matrix = read_serial()
+  if type(matrix) != list:
+    print("Bad data")
   matrix = list_to_matrix(matrix)
-  print(matrix)
-  # plot the data
+  # print(matrix)
   figure = Figure(figsize=(6, 8))
   ax = figure.subplots()
   sns.heatmap(matrix, square=True, cbar=True, ax=ax)
   return figure
   
-def redraw_figure():
-    figure = create_figure()
-    canvas.figure = figure
-    canvas.draw()
+def redraw_figure(port, baudrate):
+  if not ser.is_open:
+    open_serial(port, baudrate)
+  figure = create_figure()
+  canvas.figure = figure
+  canvas.draw()
+  #main_window.after(100, redraw_figure(port, baudrate))
+
+def btn_connect_cmd():
+  port = select_ports.get()
+  print(port)
+  baudrate = select_bauds.get()
+  print(baudrate)
+  live = check_live.state()
+  check_live.state(['disabled'])
+  #check_live.instate(['selected'])  # returns True if the box is checked
+  print(live)
+
+  redraw_figure(port, baudrate)
 
 def idle(parent,canvas):
-  """
-  Update canvas.
-  """
-  data = str(read_serial())
-  canvas.itemconfigure("text",text="+"+data+"°C")
+  print("idle")
+  if check_live.instate(['disabled','selected']):
+    redraw_figure(port, baudrate)
+    canvas.update()
+    parent.after_idle(idle,parent,canvas)
 
-  canvas.update()
-  parent.after_idle(idle,parent,canvas)
+# Global variables
 
-'''
-# Check command line arguments
-if (len(sys.argv) != 3):
-   print("command line: gui.py port baudrate") #/dev/ttyUSB0 4800
-   sys.exit()
-port = sys.argv[1]
-baudrate = int(sys.argv[2])
-
-# kas port on olemas????
-if not path.exists(port):
-  print("{} not avaible. Try the ones below:". format(port))
-  print(find_ports())
-  exit(0)
-
-print("Port: {}".format(port))
-print("Baudrate: {}".format(baudrate))
-'''
-# Open serial port
-port = '/dev/ttyUSB0'
-baudrate = 9600
-open_serial(port, baudrate)
 avaible_ports = find_ports() # find avaible ports
 avaible_baudrates = ['300','1200','2400','4800','9600','19200','38400','57600',
                     '74880','115200','230400','250000','500000','1000000','2000000']
-default_baudrate = avaible_baudrates[4]
+baudrate = avaible_baudrates[4]
+port = '/dev/ttyUSB0'
+
+create_serial()
+
 
 ####################################################
 #  set up GUI
-#################################################### 
-#WINDOW = 200 # window size
+####################################################
+
 main_window = tk.Tk()
 main_window.title("Tauno Solar Sailer")
 main_window.bind('q', 'exit')
 
-#time.sleep(1.5) # On vajalik esimesel korral!
+style = tk.ttk.Style()
+#print(style.theme_names())
+style.theme_use('clam')
+
+
+#style.configure("TButton", padding=6, relief="flat", background="#ccc")
+#style.configure("TLabel", padding=6, foreground="black", background="white")
 
 # Select Port
-label_ports = tk.Label(main_window, text="Port:", width=10, padx=5, pady=5)
-#label_ports.pack()
+label_ports = tk.ttk.Label(main_window, text="Port:", width=10)
 label_ports.grid(row=0, column=0)
 
 select_ports = tk.ttk.Combobox(textvariable='Ports', width=15)
 select_ports.insert(0,avaible_ports[0]) # Select first port
 select_ports['values'] = avaible_ports
-#select_ports.pack()
 select_ports.grid(row=0, column=1)
 
 # Select Badrate
-label_bauds = tk.Label(main_window, text="Baudrate:", width=15, padx=5, pady=5)
+label_bauds = tk.ttk.Label(main_window, text="Baudrate:", width=15)
 label_bauds.grid(row=0, column=2)
 select_bauds = tk.ttk.Combobox(textvariable='Baudrates', width=15)
 select_bauds['values'] = avaible_baudrates
-select_bauds.insert(0, default_baudrate)
+select_bauds.insert(0, baudrate)
 select_bauds.grid(row=0, column=3)
 
 # Live Checkbutton
-check_live = tk.Checkbutton(main_window, text="Stay live",  onvalue="Live", offvalue="Offline", width=15)
-check_live.grid(row=0, column=4) # command=pass,
+check_live = tk.ttk.Checkbutton(main_window, text="Stay live", width=15)
+check_live.state(['!selected'])
+check_live.grid(row=0, column=4)
 
 # Button
-button_connect = tk.Button(text="Load Data", command=redraw_figure, width=15, padx=5, pady=5)
-#button_connect.pack()
+button_connect = tk.ttk.Button(text="Load Data", command=btn_connect_cmd, width=15)
 button_connect.grid(row=0, column=5)
 
 # create empty figure and draw
 init_figure = create_init_figure()
-
 canvas = FigureCanvasTkAgg(init_figure, master=main_window)
+
 canvas.draw()
 #canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 canvas.get_tk_widget().grid(row=1, column=0, columnspan=6)
-
-#canvas = tk.Canvas(window, width=2*WINDOW, height=WINDOW, background='white')
-#canvas.create_text(WINDOW,.5*WINDOW,text="0°C",font=("Ubuntu", 36),tags="text",fill="#525252")
-#canvas.pack()
 
 # Loop
 #main_window.after(100,idle,main_window,canvas)
